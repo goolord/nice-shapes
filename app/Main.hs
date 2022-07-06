f8b967646944a496ea6d2253533660b1f51c093f
Zachary Churchill <zacharyachurchill@gmail.com>
2022-07-05 20:52:15 -0400

    shitty shape selector UI

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE DataKinds, ExtendedDefaultRules #-}
+{-# LANGUAGE MagicHash #-}
 
 module Main where
 
 import Graphics.Gloss
-import Graphics.Gloss.Interface.Environment (getScreenSize)
 import Graphics.Gloss.Interface.IO.Interact (interactIO, Event (..), Key (..), MouseButton (..), KeyState (..), Controller (..))
 -- import Vivid
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 import Control.Monad.Writer.CPS (execWriter, MonadWriter (..))
 import Data.Foldable (for_)
+import Control.Concurrent (threadDelay)
+import GHC.Base (Int (..), (+#))
 
 data Mouse = Hovering Point | MB Point MouseButton KeyState
   deriving Show
@@ -19,9 +21,21 @@ data World = World
   , soundShapes :: IntMap SoundShape
   , newShape :: Maybe Point -- first point save
   , mouse :: Mouse
+  , selectedShape :: Shape
   }
 
 type ShapeId = Int
+
+data Shape
+  = SolidCircle
+  | SolidSquare
+  | SolidTriangle
+  | SolidRope
+  | SolidLine
+  | SoundCircle
+  | SoundSquare
+  | SoundTriangle
+  deriving (Enum, Bounded, Eq)
 
 data SolidShape =
     SoundShape
@@ -29,9 +43,53 @@ data SolidShape =
   | SLine Point Point
 
 data SoundShape =
-    SCircle Float
-  | SSquare Float
-  | STriangle Float
+    SCircle GenericShape
+  | SSquare GenericShape
+  | STriangle GenericShape
+
+data GenericShape = GenericShape
+  { pos :: Point
+  , dXY :: Point
+  , size :: Float
+  }
+
+fps :: Int
+fps = 60
+
+dt :: Float
+dt = 1.0
+
+velocity :: GenericShape -> Point -> Point
+velocity gs currentAcceleration = dXY gs `sumPoints` currentAcceleration `timesPoints` (dt, dt)
+
+position :: GenericShape -> GenericShape
+position gs =
+  gs { pos = sumPoints (pos gs) (dXY gs) `timesPoints` (dt, dt) }
+
+{- |
+/Subject to fusion./
+-}
+imap :: (Int -> a -> b) -> [a] -> [b]
+imap f ls = go 0# ls
+  where
+    go i (x:xs) = f (I# i) x : go (i +# 1#) xs
+    go _ _      = []
+{-# NOINLINE [1] imap #-}
+
+shapeSelectorUI :: Shape -> Picture
+shapeSelectorUI shape = Pictures $ imap (\i x -> translate (fromIntegral i * sz) 0.0 $ shapeToButton x) [minBound..maxBound]
+  where
+  sz = 40.0
+  shp = 30.0
+  shapeToButton s = (if shape == s then color white else color black) $ rectangleWire sz sz <> case s of
+    SolidCircle   -> circleSolid (shp/2)
+    SolidSquare   -> rectangleSolid shp shp
+    SolidTriangle -> polygon [(negate $ shp/2,negate $ shp/2), (0, shp/2), (shp/2, negate $ shp/2)]
+    SolidRope     -> line [(0.0,negate $ shp/2), (0.0, shp/2)] -- todo: make squigly
+    SolidLine     -> line [(0.0,negate $ shp/2), (0.0, shp/2)] -- todo: make squigly
+    SoundCircle   -> circleSolid (shp/2)
+    SoundSquare   -> rectangleWire shp shp
+    SoundTriangle -> line [(negate $ shp/2,negate $ shp/2), (0, shp/2), (shp/2, negate $ shp/2), (negate $ shp/2,negate $ shp/2)]
 
 -- TODO: interactively add shapes to the physics simulation
 -- assign shapes to synthesizers using vivid
@@ -57,6 +115,7 @@ newWorld = World
   , soundShapes = mempty
   , newShape = Nothing
   , mouse = Hovering (0.0, 0.0)
+  , selectedShape = SolidLine
   }
 
 mousePosPt :: Mouse -> Point
@@ -71,12 +130,21 @@ main = do
     newWorld
     (\world -> do
       pure $ execWriter $ do
+        tell $ shapeSelectorUI (selectedShape world)
         tell $ case newShape world of
           Just p -> color white $ line [p, mousePosPt $ mouse world]
           Nothing -> Blank
         for_ (IntMap.elems $ solidShapes world) $ \ss ->
           tell $ case ss of
             SLine p1 p2 -> color white $ line [p1, p2]
+            Rope p1 p2 sid ->
+              let shape = IntMap.lookup sid (soundShapes world)
+                  p2' = case shape of
+                    Just (SCircle gs) -> pos gs
+                    Just (SSquare gs) -> pos gs
+                    Just (STriangle gs) -> pos gs
+                    Nothing -> error "delete this rope i guess"
+              in color white $ line [p1, sumPoints p2 p2']
     )
     (\e world -> do
       case e of
@@ -99,8 +167,10 @@ main = do
                 }
         _ -> pure world
     )
-    (\(Controller _redraw _modifyViewPort) -> do
-      -- update every n seconds
+    (\(Controller redraw _modifyViewPort) -> do
+      -- redraw every 1/60 of a second
+      threadDelay 16666
+      redraw
       -- clearCache world
       -- modifyViewPort $ \vp -> do
       --   (x, y) <- readIORef (windowSize state)
@@ -110,9 +180,13 @@ main = do
       --       , fromIntegral          $ y `div` 2
       --       )
       --   }
-      pure ()
     )
 
+sumPoints :: Point -> Point -> Point
+sumPoints (x1, y1) (x2, y2) = (x1+x2, y1+y2)
+
+timesPoints :: Point -> Point -> Point
+timesPoints (x1, y1) (x2, y2) = (x1*x2, y1*y2)
 
 insertNew :: a -> IntMap a -> IntMap a
 insertNew a im = IntMap.insert (IntMap.size im + 1) a im
